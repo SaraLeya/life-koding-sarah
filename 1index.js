d47485d50717a29fda8b7e011b448bcbab9896bd
SaraLeya <kolesnichenkova_@mail.ru>
2023-04-25 17:13:45 +0300

    modified:   1index.html modified:   1index.js 	new file:   components/loginComponent.js

--- 1index.js
+++ 1index.js
@@ -1,4 +1,5 @@
 import { getRender, postCom} from "./1api.js";
+import { renderLoginComponents } from "./components/loginComponent.js";
 
 let commentsContainer;
 const loaderStart = document.querySelector(".loaderStart");
@@ -23,28 +24,8 @@ renderApp();
 
 function renderApp() {
   const appEl = document.getElementById("app");
-if (!token) {
-  const appHtml = `
-  <div class="container">
-  
-  <div id="pass" class="add-formPass">
-          <input id="userName" type="text" class="add-form-userName" placeholder="Введите логин" />
-          <textarea id="userPass"
-            type="textarea"
-            class="add-form-userPass"
-            placeholder="Введите ваш пароль"
-            rows="1"></textarea>
-          <div class="add-form-row">
-            <button id="login-button" class="add-form-button">Войти</button>
-          </div>
-        </div>`;
-    appEl.innerHTML = appHtml;
-document.getElementById('login-button').addEventListener('click', () => {
-  token = "Bearer ksdfsksdfjfsdjk";
-  apiGet();
-});
-
-
+if (!token) { 
+  renderLoginComponents({ appEl });
     return;
 }
 
---
+++ components/loginComponent.js
@@ -0,0 +1,21 @@
+export function renderLoginComponents ({ appEl, setToken }){
+    const appHtml = `
+    <div class="container">
+    
+    <div id="pass" class="add-formPass">
+            <input id="userName" type="text" class="add-form-userName" placeholder="Введите логин" />
+            <textarea id="userPass"
+              type="textarea"
+              class="add-form-userPass"
+              placeholder="Введите ваш пароль"
+              rows="1"></textarea>
+            <div class="add-form-row">
+              <button id="login-button" class="add-form-button">Войти</button>
+            </div>
+          </div>`;
+      appEl.innerHTML = appHtml;
+  document.getElementById('login-button').addEventListener('click', () => {
+    token = "Bearer ksdfsksdfjfsdjk";
+    apiGet();
+  });
+}
