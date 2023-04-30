c4a0df672a74898f393caf70fc6d9221d40f58ba
SaraLeya <kolesnichenkova_@mail.ru>
2023-04-30 17:19:17 +0300

    modified:   1index.html modified:   components/loginComponent.js

--- components/loginComponent.js
+++ components/loginComponent.js
@@ -14,9 +14,30 @@ export function renderLoginComponents ({ appEl, setToken }){
               rows="1"></input>
             <div class="add-form-row">
               <button id="login-button" class="add-form-button">Войти</button>
+              <button id="reg" class="add-form-button">Зарегистрироваться</button>
             </div>
+
+// регистрация 
+            <div id="reg" class="add-formPass">
+            <p>Регистрация</p>
+            <input id="reg-name-input" type="text" class="add-form-userName" 
+            placeholder="Введите ваше имя"></input>
+            <input id="reg-login-input" type="text" class="add-form-userName" 
+            placeholder="Введите логин"></input>
+            <input id="reg-password"
+              type="password"
+              class="add-form-userPass"
+              placeholder="Введите ваш пароль"
+              rows="1"></input>
+            <div class="add-form-row">
+              <button id="login-button" class="add-form-button">Войти</button>
+              <button id="toggle-button" class="add-form-button">Зарегистрироваться</button>
+            </div>
+
+
           </div>`;
       appEl.innerHTML = appHtml;
+      
   document.getElementById('login-button').addEventListener('click', () => {
 const login = document.getElementById('login-input').value;
 const password = document.getElementById('password').value;
