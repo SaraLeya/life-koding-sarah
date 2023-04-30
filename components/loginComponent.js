bc28a72b27c5bf337f520017bd382b4ddd2a6ffc
SaraLeya <kolesnichenkova_@mail.ru>
2023-04-30 20:46:37 +0300

    modified:   components/loginComponent.js

--- components/loginComponent.js
+++ components/loginComponent.js
@@ -1,58 +1,39 @@
 import { loginUser } from "../1api.js";
 
-export function renderLoginComponents ({ appEl, setToken }){
-    const appHtml = `
-    <div class="container">
+export function renderLoginComponents ({ appEl, setToken, apiGet }){
+  let isLoginMode = true;
+
+const renderForm = () => {
+  const appHtml = `
+  <div class="container">
+  
+  <div id="pass" class="add-formPass">
+  <h2>${isLoginMode ? 'Форма входа' : 'Регистрация'} </h2>
+  <input id="login-input" type="text" class="add-form-userName" 
+  placeholder="Введите логин"></input>
+  <input id="password" type="password" class="add-form-userPass" placeholder="Введите ваш пароль" rows="1"></input>
+          <div class="add-form-row">
+  <button id="login-button" class="add-form-button">${isLoginMode ? 'Войти' : 'Регистрация'}</button>
+  <button id="toggle-button" class="add-form-button">${isLoginMode ? 'К регистрации' : 'Ко входу'}</button>
+          </div>
+        </div>`;
+    appEl.innerHTML = appHtml;
     
-    <div id="pass" class="add-formPass">
-            <input id="login-input" type="text" class="add-form-userName" 
-            placeholder="Введите логин"></input>
-            <input id="password"
-              type="password"
-              class="add-form-userPass"
-              placeholder="Введите ваш пароль"
-              rows="1"></input>
-            <div class="add-form-row">
-              <button id="login-button" class="add-form-button">Войти</button>
-              <button id="reg" class="add-form-button">Зарегистрироваться</button>
-            </div>
-
-// регистрация 
-            <div id="reg" class="add-formPass">
-            <p>Регистрация</p>
-            <input id="reg-name-input" type="text" class="add-form-userName" 
-            placeholder="Введите ваше имя"></input>
-            <input id="reg-login-input" type="text" class="add-form-userName" 
-            placeholder="Введите логин"></input>
-            <input id="reg-password"
-              type="password"
-              class="add-form-userPass"
-              placeholder="Введите ваш пароль"
-              rows="1"></input>
-            <div class="add-form-row">
-              <button id="login-button" class="add-form-button">Войти</button>
-              <button id="toggle-button" class="add-form-button">Зарегистрироваться</button>
-            </div>
-
-
-          </div>`;
-      appEl.innerHTML = appHtml;
-      
-  document.getElementById('login-button').addEventListener('click', () => {
+document.getElementById('login-button').addEventListener('click', () => {
 const login = document.getElementById('login-input').value;
 const password = document.getElementById('password').value;
-  if (!login)  {
-    alert('ВВЕДИТЕ ЛОГИН');
-    return;
-  }
-  if (!password)  {
-    alert('ВВЕДИТЕ ПАРОЛЬ');
-    return;
-  }
+if (!login)  {
+  alert('ВВЕДИТЕ ЛОГИН');
+  return;
+}
+if (!password)  {
+  alert('ВВЕДИТЕ ПАРОЛЬ');
+  return;
+}
 
 loginUser ({
-  login:'login',
-  password:'password',
+login:'login',
+password:'password',
 })
 .then((user) => {
 console.log(user);
@@ -60,8 +41,17 @@ setToken(`Bearer ${user.user.token}`);
 apiGet();
 })
 .catch((error) => {
-  //вывести красивый алерт 
+//вывести красивый алерт 
 alert(error.message);
-  });
 });
+});
+document.getElementById('toggle-button').addEventListener('click', () => {
+isLoginMode = !isLoginMode;
+renderForm();
+});
+};
+
+renderForm();
 }
+
+
